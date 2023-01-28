484af31482b386295b207f7cb2f4f942a8a0d053
Jack Shih <randomdize@gmail.com>
2023-01-28 23:18:30 +0800

    initial commit

---
+++ flake.nix
@@ -0,0 +1,40 @@
+{
+  # basic nix syntac
+  # inputs = { nixpkg.url = ""; home-manager.url = ""}
+  # is equivalent to
+  # inputs.nixkpg.url = "";
+  # inputs.home-manager.url = "";
+  
+  description = "A flake sample for setup with overlay";
+
+  inputs = {
+    nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-22.11-darwin";
+    home-manager.url = "github:nix-community/home-manager";
+    home-manager.inputs.nixpkgs.follows = "nixpkgs";
+    darwin.url = "github:lnl7/nix-darwin";
+    darwin.inputs.nixpkgs.follows = "nixpkgs";
+    emacs-overlay.url = "github:nix-community/emacs-overlay";
+  };
+  
+  outputs = { self, nixpkgs, home-manager, darwin, emacs-overlay }: 
+    let
+      overlays = [
+        emacs-overlay.overlay
+      ];
+    in {
+      # darwinConfiguration."your-hostname" is mantertory
+      # you can check your hostname by running `hostname` in terminal
+      # abc for abc.local for example.
+      darwinConfiguration."abc" = darwin.lib.darwinSystem {
+        system = "aarch64-darwin";
+        modules = [
+          home-manager.darwinModules.home-manager
+          {
+            nixpkgs.overlays = overlays;
+          }
+          # file name and location can be changed.
+          ./hosts/abc/default.nix
+        ];
+      };
+    };
+}
---
+++ hosts/abc/default.nix
@@ -0,0 +1,77 @@
+{ pkgs, ...}:
+{ 
+  services.nix-daemon.enable = true;
+  nix = {
+    package = pkgs.nixFlakes;
+    extraOptions = ''
+experimental-features = nix-command flakes
+'';
+  };
+
+  programs.zsh.enable = true;
+
+  home-manager.useGlobalPkgs = true;
+  home-manager.useUserPackages = true;
+  # change abc to your username
+  # check username by running `whoami` command in terminal.
+  home-manager.users.abc = { pkgs, ... }: {
+    home.stateVersion = "22.11";
+    home.packages = [
+      pkgs.emacsPgtk
+    ];
+  };
+
+  fonts = {
+    fontDir.enable = true;
+    fonts = [];
+  };
+
+  ## macOS Setting.
+  # system = {
+  #   defaults = {
+  #     NSGlobalDomain = {
+  #       _HIHideMenuBar = false;
+  #     };
+  #     dock = {
+  #       autohide = true;
+  #     };
+  #   };
+  #   keyboard = {
+  #     enableKeyMapping = true;
+  #     remapCapsLockToControl = true;
+  #   };
+  # };
+  
+  
+  homebrew = {
+    enable = true;
+    onActivation.autoUpdate = true;
+    ## uncomment the line below if you want to auto remove any formula not listed below.
+    # onActivation.cleanup = "uninstall";
+
+    brews = [
+      # "tree-sitter"
+      # {
+      #   name = "emacs-plus@30";
+      #   args = ["with-widgets"];
+      #   link = true;
+      # }
+    ];
+    casks = [
+      # "1password"
+      # "alfred"
+      # "rectangle"
+      # "font-fira-code"
+    ];
+    taps = [
+      # "d12frosted/emacs-plus"
+      # "homebrew/bundle"
+      # "homebrew/cask"
+      # "homebrew/cask-drivers"
+      # "homebrew/cask-fonts"
+      # "homebrew/core"
+      # "homebrew/services"
+    ];
+  };
+}
+ 
